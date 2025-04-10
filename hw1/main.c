5a3fdc2976044dc226e4ba16fbece9e1438edef1
YishaoVM <yishao2004@gmail.com>
2025-04-10 23:46:29 +0800

    Refactor is_alpha function for improved readability

--- hw1/main.c
+++ hw1/main.c
@@ -59,9 +59,9 @@ int is_symbol(char c) {
 }
 
 int is_alpha(char c) {
-    if (c >= 'a' && c <= 'z') return true;
-    if (c >= 'A' && c <= 'Z') return true;
-    if (c == '_') return true;
+    if ((c >= 'a' && c <= 'z') ||
+        (c >= 'A' && c <= 'Z') ||
+        (c == '_')) return true;
     return false;
 }
 
